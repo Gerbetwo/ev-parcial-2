241fce7da83f02f5647136239fc6d9f71cebe526
Gerbetwo <lassomoluna9@gmail.com>
2026-04-15 07:37:40 -0500

    feat: point 1, connection to the database working a compiling fine

---
+++ src/main/java/com/epw/tech_solution_DALM/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.epw.tech_solution_DALM.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epw/tech_solution_DALM/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epw.tech_solution_DALM.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
